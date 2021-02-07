3fce93e35140fbeaff43feb2105d4c2831b75296
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-07 09:54:47 +0100

    start using mongoose to store data and then show on frontend

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,7 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -5,9 +9,9 @@ exports.getPosts = (req, res, next) => {
         _id: '1',
         title: 'First Post',
         content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
+        imageUrl: 'images/planejpg.jpg',
         creator: {
-          name: 'Maximilian'
+          name: 'Hristijan'
         },
         createdAt: new Date()
       }
@@ -16,11 +20,33 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    const error = new Error('Validation failed');
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
-  });
+  const post = new Post({
+    title: title, 
+    content: content, 
+    imageUrl: 'images/planejpg.jpg',
+    creator: { name: 'Kiko' },
+  })
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
