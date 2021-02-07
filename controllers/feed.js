07aee3b3d9d23b5a2b6e4acc1f7aafeb72e85a8f
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-07 15:52:09 +0100

    using 'multer' to upload photos to website

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,16 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/planejpg.jpg',
-        creator: {
-          name: 'Hristijan'
-        },
-        createdAt: new Date()
+  Post.find()
+    .then(posts => {
+      res.status(200).json({ message: 'Fetched PostS', posts})
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
       }
-    ]
-  });
+      next(err);
+    })
 };
 
 exports.createPost = (req, res, next) => {
@@ -26,12 +22,18 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if(!req.file) {
+    const error = new Error('No image provider');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path.replace("\\" ,"/");
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title, 
     content: content, 
-    imageUrl: 'images/planejpg.jpg',
+    imageUrl: imageUrl,
     creator: { name: 'Kiko' },
   })
   post
@@ -50,3 +52,23 @@ exports.createPost = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error('Cloud not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200)
+        .json({ message: 'Post fetched', post })
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
