949984cacb05bf872d749f36859b29b90b0f83d9
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-10 07:01:27 +0100

    added more action with socket.io like 'delete, add, update'

--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
@@ -10,28 +10,34 @@ const router = express.Router();
 router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', isAuth, [
+router.post(
+  '/post',
+  isAuth,
+  [
     body('title')
-        .trim()
-        .isLength({ min: 5 }),
+      .trim()
+      .isLength({ min: 5 }),
     body('content')
-        .trim()
-        .isLength({ min: 5 }),
-    ], 
-    feedController.createPost
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
 );
 
 router.get('/post/:postId', isAuth, feedController.getPost);
 
-router.put('/post/:postId', [
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
     body('title')
-        .trim()
-        .isLength({ min: 5 }),
+      .trim()
+      .isLength({ min: 5 }),
     body('content')
-        .trim()
-        .isLength({ min: 5 }),
-    ], 
-    feedController.updatePost
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
 );
 
 router.delete('/post/:postId', isAuth, feedController.deletePost);
